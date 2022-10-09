b338f49f51d8ab7fd2843acdfe665056e7ab3fb5
XtremeBlaze777 <ratikmathur@gmail.com>
2022-10-09 15:19:16 -0400

    switched engine to postgresql

--- LousListA8/settings.py
+++ LousListA8/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'LousListA8.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'DATABASE_URL': os.environ['DATABASE_URL'],
 
         # TODO - will learn environment variables to mask credentials
