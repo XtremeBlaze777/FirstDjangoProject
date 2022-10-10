333f41e5d9d34d9ea49d3c07ea8a27751c658a61
srolarah <srolarah@gmail.com>
2022-10-10 12:21:51 -0400

    test 1

--- polls/tests.py
+++ polls/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
-
+from django.contrib.auth.models import User
+from django.test import Client
+from django.contrib import auth
+import os
 # Create your tests here.
 
 #Dummy test cases
@@ -10,3 +13,9 @@ class DummyTestCase(TestCase):
     
     def test_dummy_test_case(self):
         self.assertEqual(1, 1)
+
+#Google login test cases
+class googleLoginTestCase(TestCase):
+    def test_user_not_logged_in(self):
+        response = self.client.get('/polls', follow=True)
+        self.assertEqual(response.redirect_chain[0][0], "/polls/")    
