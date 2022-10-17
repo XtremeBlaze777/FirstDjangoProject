3d97e432fdb137418198f7a8666635558aae170d
Sarah <89548730+srolarah@users.noreply.github.com>
2022-10-17 18:30:29 -0400

    Test 3

--- polls/tests.py
+++ polls/tests.py
@@ -1,9 +1,13 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import Client
+from django.contrib import auth
+import os
 
 # Create your tests here.
 
-#Dummy test cases
-class DummyTestCase(TestCase):
+#setup test cases
+class setUpTestCase(TestCase):
     def setUp(self):
         x = 1
         y = 2
@@ -11,8 +15,17 @@ class DummyTestCase(TestCase):
     def test_dummy_test_case(self):
         self.assertEqual(1, 1)
         
-#Google login test cases
+#Google login test
 class googleLoginTestCase(TestCase):
     def test_user_not_logged_in(self):
         response = self.client.get('/polls', follow=True)
-        self.assertEqual(response.redirect_chain[0][0], "/polls/")    
+        self.assertEqual(response.redirect_chain[0][0], "/polls/")
+        
+#Unauthorized login
+class unauthLoginTestCase(TestCase):
+    def unauth_user_logged_in(self):
+        c = Client()
+        User.objects.create_user(username='stream', password='lg')
+        c.login(username='stream', password='lg')
+        response = self.client.get('/profiles', follow=True)
+        self.assertTrue(response.redirect_chain[0][0], "/polls/")
