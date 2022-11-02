2adb99f369e6e9740d8a107d121511dfc073d62f
CooperSc <cooperscher@MacBook-Pro.local>
2022-11-01 22:26:03 -0400

    Added Sections and Descriptions

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('subject/<str:subject>', views.subject, name='subject'),
-    path('subject/<str:subject>/course/<int:courseNum>', views.course, name='course')
+    path('subject/<str:subject>/course/<int:courseNum>', views.course, name='course'),
+    path('subject/<str:subject>/course/<int:courseNum>/section/<str:sectionNum>', views.section, name='section')
 ]
