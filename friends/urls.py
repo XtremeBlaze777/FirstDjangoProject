efb4eb799713f7842724f20e69d1ad707e1148ba
Ratik <rdm7rkm@virginia.edu>
2022-11-07 18:09:48 -0500

    Built out friends searched display page

--- friends/urls.py
+++ friends/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'friends'
+
 urlpatterns = [
-    path('', views.index, name='friends'),
+    path('', views.index, name='friendsHome'),
+    path('search/', views.SearchResultsView.as_view(), name='search_results')
 ]
--- friends/views.py
+++ friends/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.conf import settings
 
 def index(request):
     search_terms = ['admin']
@@ -7,3 +9,14 @@ def index(request):
         'search_terms' : search_terms
     }
     return render(request, 'friends.html', html_input)
+
+class SearchResultsView(ListView):
+    model = settings.AUTH_USER_MODEL
+    template_name = "user_search.html"
+
+    def get_queryset(self):  
+        query = self.request.GET.get("q")
+        object_list = Course.objects.filter(
+            Q(subject__icontains=query)
+        )
+        return object_list
