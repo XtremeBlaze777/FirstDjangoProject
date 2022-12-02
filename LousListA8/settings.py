a4024f6d5aae8c865f6b5909299940e382d59f0e
carrollJaden <102431201+carrollJaden@users.noreply.github.com>
2022-12-02 10:35:15 -0500

    SECURITY CHANGES

--- LousListA8/settings.py
+++ LousListA8/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=!r@r0%o-c5msp37#&^u)c##6v0g6^ss@gjnr_+m+ro@w^7a@3'
+SECRET_KEY = 'x!r@r0%o-c5msp37#&^u)c##6v0g6^ss@gjnr_+m+ro@w^7a@3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
